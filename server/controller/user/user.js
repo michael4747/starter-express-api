2e2fcb15dbb2b580f94fa38f6042b1bb3b1c997a
Ashish12Jangid <113776166+Ashish12Jangid@users.noreply.github.com>
2023-03-26 16:19:05 +0530

    updated

--- server/controller/user/user.js
+++ server/controller/user/user.js
@@ -136,7 +136,7 @@ exports.logIn = async (req, res) => {
                 if (userData) {
                     let password_hash = sha256(password);
                     if (userData.password == password_hash) {
-                        res.status(200).send({ success: true, msg: "Logged in successfully", data: "", errors: '' });
+                        res.status(200).send({ success: true, msg: "Logged in successfully", data: userData, errors: '' });
                     } else {
                         res.status(202).send({ success: false, msg: "Invalid Credentials", data: '', errors: '' });
                     }
